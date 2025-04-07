f5b5c600437b7677541b2fdbfbc3e5ecceb0feb0
Marwa Ayman <i.am.marwa.ayman@gmail.com>
2025-04-07 07:43:34 +0200

    fix naming / erorr handling / add go routines

--- app/main.go
+++ app/main.go
@@ -5,9 +5,10 @@ import (
 	"net"
 	"os"
 	"strings"
+	"time"
 )
 
-type request struct {
+type Request struct {
 	method  string
 	path    string
 	version string
@@ -15,27 +16,65 @@ type request struct {
 	body    string
 }
 
-func NewRequest(b []byte) *request {
-	req := &request{}
-	ele := strings.Split(string(b), "\r\n")
-	startLine := strings.Split(ele[0], " ")
-
-	req.method = startLine[0]
-	req.path = startLine[1]
-	req.version = startLine[2]
-	header := make(map[string]string)
-	for _, i := range ele {
+func NewRequest(data []byte) *Request {
+	lines := strings.Split(string(data), "\r\n")
+	if len(lines) == 0 {
+		return nil
+	}
+	requestLine := strings.Split(lines[0], " ")
+	if len(requestLine) != 3 {
+		return nil
+	}
+	req := &Request{
+		method:  requestLine[0],
+		path:    requestLine[1],
+		version: requestLine[2],
+		header:  make(map[string]string),
+	}
+	for _, i := range lines {
+		if i == "" {
+			break // empty line end of header
+		}
 		mapEle := strings.Split(i, ": ")
 		if len(mapEle) == 2 {
 			key := strings.ToLower(mapEle[0])
 			value := mapEle[1]
-			header[key] = value
+			req.header[key] = value
 		}
 	}
-	req.header = header
 	return req
 }
+func GetRespond(req *Request) string {
+	respond := "HTTP/1.1 200 OK\r\n\r\n"
+	if strings.HasPrefix(req.path, "/echo") {
+		time.Sleep(time.Second * 5)
+		str := req.path[6:]
+		respond = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(str), str)
+	} else if strings.HasPrefix(req.path, "/user-agent") {
+		time.Sleep(time.Second * 1)
+		str := req.header["user-agent"]
+		respond = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(str), str)
+	} else if req.path != "/" {
+		time.Sleep(time.Second * 10)
+		respond = "HTTP/1.1 404 Not Found\r\n\r\n"
+	}
+	return respond
+}
+func HandleConnction(conn net.Conn) {
+	defer conn.Close()
 
+	buffer := make([]byte, 1024)
+	n, err := conn.Read(buffer)
+	if err != nil {
+		return
+	}
+	req := NewRequest(buffer[:n])
+	if req == nil {
+		return
+	}
+	respond := GetRespond(req)
+	conn.Write([]byte(respond))
+}
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
@@ -46,24 +85,12 @@ func main() {
 		os.Exit(1)
 	}
 	defer l.Close()
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go HandleConnction(conn)
 	}
-	reader := make([]byte, 1024)
-	conn.Read(reader)
-	req := NewRequest(reader)
-	respond := "HTTP/1.1 200 OK\r\n\r\n"
-	if strings.HasPrefix(req.path, "/echo/") {
-		str := req.path[6:]
-		respond = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(str), str)
-	} else if strings.HasPrefix(req.path, "/user-agent") {
-		str := req.header["user-agent"]
-		respond = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(str), str)
-	} else if req.path != "/" {
-		respond = "HTTP/1.1 404 Not Found\r\n\r\n"
-	}
-	conn.Write([]byte(respond))
-	conn.Close()
 }
